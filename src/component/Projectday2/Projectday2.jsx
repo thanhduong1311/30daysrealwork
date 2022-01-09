67dafb1ef9576bd34d1d63c31f3d4a913ace71ee
thanhduong1311 <thanhduongjnguyen@gmail.com>
2022-01-09 19:29:18 +0700

    card

--- src/component/Projectday2/Projectday2.jsx
+++ src/component/Projectday2/Projectday2.jsx
@@ -6,13 +6,13 @@ export default function Projectday2() {
          <div className='Projectday2'>
              <div className='card-container'>
                  <div className='card-container__avatar'>
-                     <img src="https://1.bigdata-vn.com/wp-content/uploads/2021/09/1633017412_636_Hinh-anh-meo-con-dep.jpg" alt="" />
+                     <img src="https://scontent.fvca1-2.fna.fbcdn.net/v/t39.30808-6/239728471_605182510651633_3408156484328906353_n.jpg?_nc_cat=104&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=IOYyM-aQA4QAX86zy8E&_nc_oc=AQnjwsrxFVwJbWWZWqb9KuQwX9khVLTWdZo2r9O5N8bodaWeQ4XooQ5yvHOktyXgYjI&_nc_ht=scontent.fvca1-2.fna&oh=00_AT_39mbdbkKTt_8K_vLht4lAnBeULs_xw9mBY1AkjxnEIw&oe=61DF423E" alt="" />
                  </div>
                  <h3 className='card-container__name'>
                      Thanh Duong
                  </h3>
                  <h4 className='card-container__exp'>
-                     Full Stack Developer
+                 Tech Manager
                  </h4>
                  <div className='card-container__contact'>
                 <a to=""> <i class="fab fa-facebook-f"></i></a>
@@ -27,13 +27,14 @@ export default function Projectday2() {
 
              <div className='card-container'>
                  <div className='card-container__avatar'>
-                     <img src="https://1.bigdata-vn.com/wp-content/uploads/2021/09/1633017412_636_Hinh-anh-meo-con-dep.jpg" alt="" />
+                     <img src="https://scontent.fvca1-3.fna.fbcdn.net/v/t39.30808-6/245951286_2961723444079304_8553420563356555534_n.jpg?_nc_cat=103&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=yUGTFnXU7SEAX-mUuPt&_nc_ht=scontent.fvca1-3.fna&oh=00_AT8BjciYSALMDenONWuI7JTigQW61l9r5Xs45PcXmAAQGA&oe=61DF448F" alt="" />
                  </div>
                  <h3 className='card-container__name'>
-                     Thanh Duong
+                     Thanh Toan
                  </h3>
                  <h4 className='card-container__exp'>
-                     Full Stack Developer
+                 Full Stack Developer
+                     
                  </h4>
                  <div className='card-container__contact'>
                 <a to=""> <i class="fab fa-facebook-f"></i></a>
@@ -48,13 +49,13 @@ export default function Projectday2() {
 
              <div className='card-container'>
                  <div className='card-container__avatar'>
-                     <img src="https://1.bigdata-vn.com/wp-content/uploads/2021/09/1633017412_636_Hinh-anh-meo-con-dep.jpg" alt="" />
+                     <img src="https://scontent.fvca1-3.fna.fbcdn.net/v/t39.30808-1/p200x200/271305711_1595693424100933_4960599513470815582_n.jpg?_nc_cat=103&ccb=1-5&_nc_sid=7206a8&_nc_ohc=1JO90dR5qjoAX-KhR_T&_nc_ht=scontent.fvca1-3.fna&oh=00_AT_BovrdoESml9qm6792jPW4KzBfXLK-SpdyrHOUCXl8FQ&oe=61DEE1B7" alt="" />
                  </div>
                  <h3 className='card-container__name'>
-                     Thanh Duong
+                     Khoi Pham
                  </h3>
                  <h4 className='card-container__exp'>
-                     Full Stack Developer
+                     Profestional Design
                  </h4>
                  <div className='card-container__contact'>
                 <a to=""> <i class="fab fa-facebook-f"></i></a>
