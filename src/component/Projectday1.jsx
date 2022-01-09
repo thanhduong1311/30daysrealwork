c819638b21ea5b2181c69dad4da646f7c37121f6
BTWThanhToan <tranthanhtoanhdkg@gmail.com>
2022-01-09 11:51:09 +0700

    ĐÔI GIAY

--- src/component/Projectday1.jsx
+++ src/component/Projectday1.jsx
@@ -5,26 +5,45 @@ export default function Projectday1() {
     return (
 
     <>
-      <div className='test'>
-          <ul >
-              <li>1</li>
-              <li>2</li>
-              <li>3</li>
-              <li>4</li>
-              <li>5</li>
-              <li>6</li>
-              <li>7</li>
-              <li>8</li>
-          </ul>
-          <ul>
-              <li>6</li>
-              <li>7</li>
-              <li>8</li>
-              <li>8</li>
-              <li>8888</li>
-          </ul>
-      </div>
+    <div className="main">
+    <div class="card">
+            <div class="card-icon">
+                <i class="ti-heart"></i>
+                <i class="ti-shopping-cart"></i>
+            </div>
+            <div class="card-img">
+                <img class="img-shoes1" src="https://www.nodemy.vn/projects/html-css-js/product-card/Nike%20Zoom%20KD%2012.png" alt=""/>
+            </div>
+            <div class="card-title">
+                Nike-Pro-Plus
+            </div>
+            <div class="card-price">
+                3000$
+            </div>
+            <div class="card-size">
+               <h3>Size:</h3>
+                <span>1</span>
+                <span>2</span>
+                <span>3</span>
+                <span>4</span>
+            </div>
+            <div class="card-color">
+            <h3>Color:</h3>
+                <span class="red"></span>
+                <span class="green"></span>
+                <span class="blue"></span>
+
+            </div>
+            <div class="card-action">
+                <button class="buy-btn">Buy now</button>
+                <button class="add-btn">Add Cart</button>
+            </div>
+        </div>
+  
+    </div>
     </>
+    
+     
     
  )
 }
