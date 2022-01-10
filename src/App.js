2efc212db2eb45ab746119d9699d96a4b66dec0e
thanhduong1311 <thanhduongjnguyen@gmail.com>
2022-01-10 07:28:01 +0700

    create PD3

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import Projectday1 from "./component/Projectday1";
 import Projectday2 from "./component/Projectday2/Projectday2";
+import Projectday3 from "./component/Projectday3/Projectday3";
 function App() {
   // return  <Projectday1/>
-  return <Projectday2/>
-  
+  // return <Projectday2/>
+  return <Projectday3/>
   
 }
 
---
+++ src/component/Projectday3/Projectday3.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./asset/Projectday3.css"
+export default function Projectday3() {
+    return (
+        <div>
+           
+        </div>
+    )
+}
