3ae0aef3f3e4e261e1c60e0fb7939e2c906d7b7f
thanhduong1311 <thanhduongjnguyen@gmail.com>
2022-01-10 08:13:42 +0700

    chua co js JD3

--- src/component/Projectday3/Projectday3.jsx
+++ src/component/Projectday3/Projectday3.jsx
@@ -2,8 +2,33 @@ import React from 'react'
 import "./asset/Projectday3.css"
 export default function Projectday3() {
     return (
-        <div>
-           
+        <div className="Projectday3">
+               <div className="modal__open-btn">
+                   Open Modal
+               </div>
+           <div className="modal ">
+               <div className="modal__container">
+                      <div className="modal__container-header">
+                          <span className="modal__container-header-name">Welcome to Nodemy</span>
+                          <span className="close-icon">
+                          <i class="fas fa-times"></i>
+                          </span>
+                      </div>
+                      <div className="modal__container-body">
+                          <div className="modal__container-body-content">
+                              <h4 className="modal__container-body-content-header">Modal</h4>
+                              <p className="modal__container-body-content-body">Đây là phần body của modal</p>
+                          </div>
+                          <div className="modal__container-body-footer">
+                              <button className="modal__container-body-footer-btn">
+                                    Close
+                              </button>
+                          </div>
+                      </div>
+               </div>
+           </div>
+        
+
         </div>
     )
 }
